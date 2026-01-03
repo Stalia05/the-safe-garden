7fc39e091b1219d935dc615b4ad34f80f8c2c10a
Stalia05 <linaaa856@gmail.com>
2026-01-03 18:38:29 +0100

    Create ancrage.js

---
+++ ancrage.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("validate");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+  message.textContent =
+    "Bien joué 🌿 Tu es ici, maintenant. Prends une respiration.";
+});
